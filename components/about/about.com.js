d917e2a0e16f5ae3ebfa26101da7c660715a02db
Danieltrue <dannytrue75@gmail.com>
2022-09-28 04:14:28 -0700

    added the about

--- components/about/about.com.js
+++ components/about/about.com.js
@@ -3,7 +3,18 @@ import Aboutstyle from "./about.css";
 const About = () => {
     return <Aboutstyle>
             <div className="textbox-">
-                <main className="box">chanmpdad</main>
+                <main className="box">
+                    <p>I am Daniel okezie a fullstack developer who studied marine engineering,
+                        i take good joy in building things on the web i literally still watch cartoon, i'm
+                        always happy with myself even during the difficult times that's why my friends and family still
+                        don't understand who i am, life has given me 19+ years of experience that i'm not in a hurry to forget
+                        on thing that made me more happy was when i started programming in 2020 before the pandemic i learnt HTML
+                        and CSS then built my first page from there i began learning more on programming and today i'm able 
+                        to build a full web application from scratch it's wonderful to just have this power in my hands and computer
+                        everyday i wish i could build applications.
+                    </p>
+                    <h6>Daniel okezie</h6>
+                </main>
             </div>
         </Aboutstyle>
     
--- components/about/about.css.js
+++ components/about/about.css.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 const Aboutstyle = styled.section`
-    margin-top: 600px;
+    margin-top: 400px;
     display: flex;
     justify-content: center;
     align-items:  center;
+    padding: 1rem;
+    .textbox- {
+        width: 700px;
+        p {
+            font-family: var(--san-serif);
+            font-size: 16px;
+            line-height: 1.5;
+        }
+        h6 {
+            font-size: 16px;
+            font-family: var(--san-serif);
+            margin-top: 16px;
+        }
+    }
 `;
 
 export default Aboutstyle;
